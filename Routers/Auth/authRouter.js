65dff71ff7c04f7b315378717e076dcdf5913715
moshaosama <mohamedOSFekry@gmail.com>
2025-07-26 01:16:08 +0300

    refactor(router): update authRouter routes

--- Routers/Auth/authRouter.js
+++ Routers/Auth/authRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { SignUp } from "../../Controller/Auth/signUp.js";
 import { Validate } from "../../Middleware/Validates.js";
-import { SignUpSchema } from "../../Schemas/UserSchemas.js";
+import { LoginSchema, SignUpSchema } from "../../Schemas/UserSchemas.js";
 import { setBcryption } from "../../Middleware/bcryption.js";
+import { Login } from "../../Controller/Auth/login.js";
 
 const router = express.Router();
 
-router.route("/").post(Validate(SignUpSchema), setBcryption, SignUp);
+router.route("/signup").post(Validate(SignUpSchema), setBcryption, SignUp);
+router.route("/login").post(Login);
 
 export default router;
