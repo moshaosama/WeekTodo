19c7c8a7936dd0668bc669c895532a63a400048a
moshaosama <mohamedOSFekry@gmail.com>
2025-07-25 03:19:10 +0300

    refactor: update logic in index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+const app = express();
+const Port = process.env.PORT;
+
+// Middleware
+app.use(express.json());
+
+app
+  .listen(Port)
+  .on("listening", () => {
+    console.log(`listening successfully on port ${Port}`);
+  })
+  .on("error", (err) => {
+    console.log(err);
+  });
