c3540f33084f501ac86f0ee6f939d5c06800eb6e
moshaosama <mohamedOSFekry@gmail.com>
2025-07-25 15:29:57 +0300

    feat(auth): update authentication routes

--- Routers/Auth/authRouter.js
+++ Routers/Auth/authRouter.js
@@ -1,5 +1,11 @@
 import express from "express";
+import { SignUp } from "../../Controller/Auth/signUp.js";
+import { Validate } from "../../Middleware/Validates.js";
+import { SignUpSchema } from "../../Schemas/UserSchemas.js";
+import { setBcryption } from "../../Middleware/bcryption.js";
 
 const router = express.Router();
+
+router.route("/").post(Validate(SignUpSchema), setBcryption, SignUp);
 
 export default router;
