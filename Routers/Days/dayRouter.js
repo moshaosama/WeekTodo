44e6dd05157029a0597e4867b0697a109f8964c8
moshaosama <mohamedOSFekry@gmail.com>
2025-07-26 01:16:08 +0300

    refactor(router): update dayRouter routes

--- Routers/Days/dayRouter.js
+++ Routers/Days/dayRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { createDays } from "../../Controller/Days/createDays.js";
+import { GetDays } from "../../Controller/Days/GetDays.js";
 
 const router = express.Router();
 
-router.route("/").post(createDays);
+router.route("/create_days").post(createDays);
+router.route("/:user_id").get(GetDays);
 
 export default router;
