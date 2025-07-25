4050d214eddbf82e2ea5fae927ffba316e9b3091
moshaosama <mohamedOSFekry@gmail.com>
2025-07-25 04:17:39 +0300

    feat(constants): add constants module

---
+++ Constants/Days.js
@@ -0,0 +1,9 @@
+export const Days = [
+  { day: "Friday" },
+  { day: "Saturday" },
+  { day: "Sunday" },
+  { day: "Monday" },
+  { day: "Tuesday" },
+  { day: "Wednesday" },
+  { day: "Thursday" },
+];
