3229e686130593b002a46c07c7d39fbaf00389c5
moshaosama <mohamedOSFekry@gmail.com>
2025-07-26 20:09:03 +0300

    feat(tasks): update tasks router with new endpoints

--- Routers/Tasks/tasksRouter.js
+++ Routers/Tasks/tasksRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
+import { CreateTask } from "../../Controller/Tasks/CreateTask.js";
+import { Validate } from "../../Middleware/Validates.js";
+import { createTaskSchema, GetTaskSchema } from "../../Schemas/TaskSchema.js";
+import { GetTask } from "../../Controller/Tasks/GetTask.js";
 
 const router = express.Router();
+
+router.route("/create-task").post(Validate(createTaskSchema), CreateTask);
+router.route("/get-task/:user_id").get(Validate(GetTaskSchema), GetTask);
 
 export default router;
