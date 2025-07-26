d6173e29637649834e24bfed8dc17fa3612afdb7
moshaosama <mohamedOSFekry@gmail.com>
2025-07-26 20:09:03 +0300

    fix(auth): enhance signup controller validation

--- Controller/Auth/signUp.js
+++ Controller/Auth/signUp.js
@@ -1,5 +1,6 @@
+import { Days } from "../../Constants/Days.js";
 import { DB } from "../../utils/connectdataBase.js";
-import { User_Key } from "../../utils/CreateKey.js";
+import { Days_Details_Key, Days_Key, User_Key } from "../../utils/CreateKey.js";
 import { initialConnectionRedis } from "../../utils/rediusConnection.js";
 import { errorResponse, successResponse } from "../../utils/responses.js";
 
@@ -15,6 +16,19 @@ export const SignUp = async (req, res) => {
 
     const user_Key = User_Key(result.insertId);
     await client.hSet(user_Key, data);
+
+    const DaysKey = Days_Key(result.insertId);
+    const Days_DetailsKey = Days_Details_Key();
+    const Value = Days.map((d) => d.day);
+    const Query = "INSERT INTO days (dayName, user_id) VALUES (?,?)";
+
+    await Promise.all(
+      Days.map(async ({ day }) => {
+        const [res] = await DB.promise().query(Query, [day, result.insertId]);
+        await client.rPush(Days_DetailsKey, String(res.insertId));
+      })
+    );
+    await client.rPush(DaysKey, Value);
     successResponse(res, "addedd Successfully", data);
   } catch (err) {
     errorResponse(res, 500, err.message);
