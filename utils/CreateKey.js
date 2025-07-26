0f3b34110d71392593d7648240adbb7c84c26f23
moshaosama <mohamedOSFekry@gmail.com>
2025-07-26 20:09:04 +0300

    refactor(utils): optimize CreateKey utility

--- utils/CreateKey.js
+++ utils/CreateKey.js
@@ -3,4 +3,7 @@ const CreateKeyRedis = (...args) => {
 };
 
 export const Days_Key = (id) => CreateKeyRedis("days", id);
+export const Days_Details_Key = () => CreateKeyRedis("days-details");
 export const User_Key = (id) => CreateKeyRedis("user", id);
+
+export const Task_key = (id) => CreateKeyRedis("tasks", id);
