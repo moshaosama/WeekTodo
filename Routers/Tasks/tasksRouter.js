b6d2650209305697b0d9379714501ae0c43797fc
moshaosama <mohamedOSFekry@gmail.com>
2025-07-28 17:12:03 +0300

    feat(router): add route for fetching task by ID

--- Routers/Tasks/tasksRouter.js
+++ Routers/Tasks/tasksRouter.js
@@ -3,10 +3,12 @@ import { CreateTask } from "../../Controller/Tasks/CreateTask.js";
 import { Validate } from "../../Middleware/Validates.js";
 import { createTaskSchema } from "../../Schemas/TaskSchema.js";
 import { GetTask } from "../../Controller/Tasks/GetTask.js";
+import { getTaskByid } from "../../Controller/Tasks/getTaskById.js";
 
 const router = express.Router();
 
 router.route("/create-task").post(Validate(createTaskSchema), CreateTask);
 router.route("/get-task/:user_id/:day_id").get(GetTask);
+router.route("/get-task-by-id/:day_id/:task_id").get(getTaskByid);
 
 export default router;
