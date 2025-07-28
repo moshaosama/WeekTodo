0c24f1f9417e6512a147635a40a8761c96a7d1b1
moshaosama <mohamedOSFekry@gmail.com>
2025-07-28 18:00:17 +0300

    feat(router): add route for editing task

--- Routers/Tasks/tasksRouter.js
+++ Routers/Tasks/tasksRouter.js
@@ -4,11 +4,13 @@ import { Validate } from "../../Middleware/Validates.js";
 import { createTaskSchema } from "../../Schemas/TaskSchema.js";
 import { GetTask } from "../../Controller/Tasks/GetTask.js";
 import { getTaskByid } from "../../Controller/Tasks/getTaskById.js";
+import { EditTask } from "../../Controller/Tasks/EditTask.js";
 
 const router = express.Router();
 
 router.route("/create-task").post(Validate(createTaskSchema), CreateTask);
 router.route("/get-task/:user_id/:day_id").get(GetTask);
 router.route("/get-task-by-id/:day_id/:task_id").get(getTaskByid);
+router.route("/edit-task/:day_id/:task_id").put(EditTask);
 
 export default router;
