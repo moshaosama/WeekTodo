bba8dad52cbc380e6c0d27e57c49c972555b493b
moshaosama <mohamedOSFekry@gmail.com>
2025-07-30 16:05:28 +0300

    Update customRouter.js

--- Routers/customTask/customRouter.js
+++ Routers/customTask/customRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { CreateCustomTask } from "../../Controller/CustomTask/CreateCustomTask.js";
 import { GetCustomTasks } from "../../Controller/CustomTask/GetCustomTasks.js";
+import { DeleteCustomTask } from "../../Controller/CustomTask/DeleteCustomTask.js";
 
 const router = express.Router();
 
 router.route("/create-custom-task").post(CreateCustomTask);
 router.route("/get-custom-task").get(GetCustomTasks);
+router.route("/delete-custom-task/:customTask_id").delete(DeleteCustomTask);
 
 export default router;
